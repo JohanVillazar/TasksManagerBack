8c84075063208ea43c0ddfc21be74b35b80db8ad
unknown <villazarjohan@gmail.com>
2025-03-18 21:02:03 -0500

    taskback

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ import { adminMiddleware } from "./middlewares/adminmiddleware.js";
 const app = express();
 app.use(express.json());
 app.use(cors(
-    
-    origin=["http://localhost:5173", "https://task-manager-fawn-eight.vercel.app/"]
+
+    origin=["http://localhost:5173", "https://task-manager-ten-rose-74.vercel.app/"]
 
 ));
 
